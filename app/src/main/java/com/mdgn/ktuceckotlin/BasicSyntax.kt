d4c8a8a38f17fc1153cdbb4f2b451fbc900e7dcc
mucahid <mucaidd3@gmail.com>
2024-11-04 22:09:01 +0300

    add basic syntax

---
+++ app/src/main/java/com/mdgn/ktuceckotlin/BasicSyntax.kt
@@ -0,0 +1,95 @@
+package com.mdgn.ktuceckotlin
+
+fun main() {
+
+}
+
+/*
+        Variables
+    var name = "Ahmet"
+    name = "Mehmet"
+
+    val age = 26
+    age = 27
+
+         Data Types
+    val number : Int = 10
+    val pi: Double = 3.14
+    val weight: Float = 65.5f
+
+    val letter: Char = 'A'
+
+    val message: String = "Hello World"
+
+    val isKotlinFun: Boolean = false
+
+    val number2 = "35"
+
+        String Templates
+    val name = "Afra"
+    println("Hello Kotlin - $name")
+
+    val age = 26
+    println("My age is ${age + 2}")
+
+    var age = 26
+    println("My age is $age")
+    age += 2
+    println("My age is $age")
+
+    println("$name $age")
+
+        IF Condition
+    val number = 0
+
+    if (number > 0) {
+        println("Pozitif")
+    } else {
+        println("Negatif yada 0")
+    }
+        println("----------------------")
+    if (number > 0) {
+        println("Pozitif")
+    } else if (number < 0) {
+        println("Negatif")
+    } else {
+        println("Sifir")
+    }
+    println("----------------------")
+    val numberTwo = 10
+    val max = if (numberTwo > 5) numberTwo else 5
+    println("Max = $max")
+    println("----------------------")
+
+        When
+         val dayCount = 2
+
+    val dayName = when(dayCount) {
+        1 -> "Pazartesi"
+        2 -> "Sali"
+        3 -> "Carsamba"
+        4 -> "Persembe"
+        5 -> "Cuma"
+        6 -> "Cumartesi"
+        7 -> "Pazar"
+        else -> "Gecersiz Gun"
+    }
+
+    println("Haftanin gunu = $dayName")
+    println("----------------------")
+
+    val number = 20
+    when {
+        number < 0 -> println("Negatif")
+        number > 0 -> println("Pozitif")
+        number == 0 -> println("Sifir")
+    }
+
+    println("----------------------")
+
+    val char = 'A'
+    when(char) {
+        'A','E','I' -> println("Sesli harf")
+        else -> println("Sessiz harf")
+    }
+*/
