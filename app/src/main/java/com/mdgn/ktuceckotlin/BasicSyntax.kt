10a947e59bb847d7f1e03614994a01640ebcef23
mucahid <mucaidd3@gmail.com>
2024-11-25 23:05:59 +0300

    add functions

--- app/src/main/java/com/mdgn/ktuceckotlin/BasicSyntax.kt
+++ app/src/main/java/com/mdgn/ktuceckotlin/BasicSyntax.kt
@@ -1,10 +1,22 @@
 package com.mdgn.ktuceckotlin
 
 fun main() {
+    val number1 = 5
+    val number2 = 6
 
+    println(number1 < number2)
 }
 
 /*
+
+==	Equal to	x == y
+!=	Not equal	x != y
+>	Greater than	x > y
+<	Less than	x < y
+>=	Greater than or equal to	x >= y
+<=	Less than or equal to	x <= y
+
+
         Variables
     var name = "Ahmet"
     name = "Mehmet"
